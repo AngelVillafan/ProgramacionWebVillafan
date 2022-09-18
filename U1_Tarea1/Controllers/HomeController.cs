ace37b40856bda6278965eab53f9127f5ca41de4
191G0239@rcarbonifera.tecnm.mx <191G0239@rcarbonifera.tecnm.mx>
2022-09-18 04:15:34 -0500

    Lista tarea 1

---
+++ U1_Tarea1/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using U1_Tarea1.Models;
+
+namespace U1_Tarea1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult MiPerfil(Alumno alumno)
+        {
+            if (string.IsNullOrWhiteSpace(alumno.Nombre) || string.IsNullOrWhiteSpace(alumno.NombreMateria) || string.IsNullOrWhiteSpace(alumno.Periodo))
+            {
+                return RedirectToAction("Index");
+            }
+            return View(alumno);
+        }
+    }
+}
---
+++ U1_Tarea1/Models/Alumno.cs
@@ -0,0 +1,11 @@
+﻿namespace U1_Tarea1.Models
+{
+    public class Alumno
+    {
+        public string Nombre { get; set; } = "";
+        public string NombreMateria { get; set; } = "";
+        public int Semestre { get; set; }
+        public string Periodo { get; set; } = "";
+        //nombre de la materia, semestre, periodo y nombre del alumno.
+    }
+}
