eb94db600b320ddea2bffc8c364aa8dd6c4e5b75
191G0239@rcarbonifera.tecnm.mx <191G0239@rcarbonifera.tecnm.mx>
2022-09-18 22:04:42 -0500

    Lista tarea 2

---
+++ U1_Tarea2/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using U1_Tarea2.Models;
+
+namespace U1_Tarea2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(Promedio promedio)
+        {
+            if (!string.IsNullOrWhiteSpace(promedio.Materia1.ToString()) || !string.IsNullOrWhiteSpace(promedio.Materia2.ToString()) || !string.IsNullOrWhiteSpace(promedio.Materia3.ToString()))
+            {
+                promedio.MediaAritmetica = (promedio.Materia1 + promedio.Materia2 + promedio.Materia3) / 3;
+            }
+            return View(promedio);
+        }
+    }
+}
---
+++ U1_Tarea2/Models/Promedio.cs
@@ -0,0 +1,11 @@
+﻿namespace U1_Tarea2.Models
+{
+    public class Promedio
+    {
+        public double Materia1 { get; set; }
+        public double Materia2 { get; set; }
+        public double Materia3 { get; set; }
+        public double MediaAritmetica { get; set; }
+
+    }
+}
